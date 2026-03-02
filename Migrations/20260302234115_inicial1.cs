a8d8620d34d04f665ad8fe50978cf20b1e44aed9
Tino3q <Hanlexgarcia31@gmail.com>
2026-03-02 19:54:56 -0400

    Se Han Hecho Algunas Correciones

---
+++ Migrations/20260302234115_inicial1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hamlet_GarciaAP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "NavesEspaciales",
+                columns: table => new
+                {
+                    NaveId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Costo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NavesEspaciales", x => x.NaveId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NavesEspaciales");
+        }
+    }
+}
